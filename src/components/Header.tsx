47547f81530de5b6e18dac5cb505ad431e36adbe
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2023-10-19 17:15:36 +0600

    header section complete

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='my-5'>
+      <h1 className='mb-0 text-center text-barlow-500 site__title'>
+        Spaceflight details
+      </h1>
+      <p className='mb-0 text-center text-barlow-400 site__sub-title'>
+        Find out the elaborate features of all the past big spaceflights.
+      </p>
+    </header>
+  );
+};
+
+export default Header;
