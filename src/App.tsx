be9df19f59af73009467ba0c42a570de729628f2
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2023-10-19 19:58:54 +0600

    designed card component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,21 @@
 import React, { useEffect, useState } from 'react';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
+import Flight from './components/Flight';
 
 const App = () => {
   return (
     <div>
       <Header />
       <SearchBar />
+      <div className='container flight__wrapper'>
+        <div className='row'>
+          <Flight />
+          <Flight />
+          <Flight />
+          <Flight />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/Flight.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Flight = () => {
+  return (
+    <div className='col-lg-4'>
+      <div className='card flight__card justify-content-center align-items-center'>
+        <div className='flight__card-header'>
+          <img
+            src='https://images2.imgbox.com/40/e3/GypSkayF_o.png'
+            alt='img'
+          />
+        </div>
+        <div className='flight__card-body text-center d-flex flex-column'>
+          <div>
+            <p className='mb-0 text-barlow-400 '>
+              Launch Date: <span className='launch__date'>21 Feb, 2007</span>
+            </p>
+
+            <h2 className='mb-0 text-barlow-500 flight__title'>DemoSat</h2>
+            <p className='mb-0 text-barlow-400 flight__version'>Falcon 1</p>
+          </div>
+          <div>
+            <p className='mb-0 text-barlow-500 launch-text'>Launch Status:</p>
+            <span className='flight__status-success'>Failed</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Flight;
